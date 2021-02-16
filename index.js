1689e7e6ee8c12187c439570f1458cb9dfb16c1d
ravi002909 <66548045+ravi002909@users.noreply.github.com>
2021-02-16 10:57:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,23 @@
   doc.contentEditable = true;
   doc.focus();
 })()
+
+var id = getUrlParameter('id');
+  if (!id) {
+    location.search = location.search
+      ? '&id=' + getUniqueId() : 'id=' + getUniqueId();
+    return;
+  }
+
+// a unique random key generator
+  function getUniqueId () {
+    return 'private-' + Math.random().toString(36).substr(2, 9);
+  }
+
+  // function to get a query param's value
+  function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  };
